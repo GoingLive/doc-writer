d0121e90df744bde1997f48516cb18cc552a3003
GoingLive <roland.huettmann@gmail.com>
2026-04-24 21:06:56 +0200

    Add placeholder public index

---
+++ public/index.php
@@ -0,0 +1,13 @@
+﻿<?php
+declare(strict_types=1);
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>doc-writer</title>
+</head>
+<body>
+    <h1>doc-writer</h1>
+    <p>Basic PHP page is running.</p>
+</body>
+</html>
